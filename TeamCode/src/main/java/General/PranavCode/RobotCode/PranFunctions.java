a9e9ec6da0a8b95d82d76b2f7d4aad258bdb2439
Pranpran3 <pranavleaps@gmail.com>
2024-12-02 20:56:22 -0800

    uhhhhh

---
+++ TeamCode/src/main/java/General/PranavCode/RobotCode/PranFunctions.java
@@ -0,0 +1,15 @@
+package General.PranavCode.RobotCode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class PranFunctions {
+    public class init {
+        DcMotor fl = null;
+        DcMotor fr = null;
+        DcMotor bl = null;
+        DcMotor br = null;
+        Servo servo1 = null;
+
+    }
+}
